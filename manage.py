6064b114fa0ddd36ac82fbf73892a73d31473469
dzikrirazzan <dzikrirazzan02@gmail.com>
2025-06-29 01:22:05 +0700

    fix: Update DJANGO_SETTINGS_MODULE in manage.py for new project structure

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'turnover_prediction.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.turnover_prediction.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
