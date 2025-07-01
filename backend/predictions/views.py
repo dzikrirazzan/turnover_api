c74bfe42f19bbc367fe0672c0f5667028af4e9f0
dzikrirazzan <dzikrirazzan02@gmail.com>
2025-07-01 18:56:03 +0700

    feat: Change DELETE endpoint responses to 200 OK with success message

--- backend/predictions/views.py
+++ backend/predictions/views.py
@@ -514,6 +514,11 @@ class MLModelViewSet(viewsets.ModelViewSet):
     queryset = MLModel.objects.all()
     serializer_class = MLModelSerializer
     permission_classes = [IsAuthenticated]
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response({'message': 'Resource deleted successfully'}, status=status.HTTP_200_OK)
     
     @action(detail=False, methods=['post'])
     def train(self, request):
